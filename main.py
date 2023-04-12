12f89c27329023eacde59ff9bd775e4e0b04c519
Michael Primavera <pastasauce59@gmail.com>
2023-04-12 00:13:12 -0400

    Created 'count_down' function for the countdown mechanism of the app that caluclates minutes and seconds to count down from, created 'start_timer' function to be called from the start button and begin the acutal countdown.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,8 +14,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_minute = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count > 0:
+        canvas.itemconfig(timer_text, text=f"{count_minute}:{count_sec}")
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,14 +35,14 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 132, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 132, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
 timer_label = Label(bg=YELLOW, fg=GREEN)
 timer_label.config(text='Timer', font=(FONT_NAME, 35, 'bold'))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(highlightbackground=YELLOW)
+start_button = Button(highlightbackground=YELLOW, command=start_timer)
 start_button.config(text='Start')
 start_button.grid(column=0, row=2)
 
