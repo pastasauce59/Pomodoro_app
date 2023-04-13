9a5018c948ce06ecb66e3b0a1a7eb0ecc93d4efe
Michael Primavera <pastasauce59@gmail.com>
2023-04-12 23:59:57 -0400

    created logic for setting the start_timer to check reps for which amount of time should be placed in timer, work time or break time.

--- main.py
+++ main.py
@@ -7,15 +7,33 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    #If its the 1st/3rd/5th/7th rep:
+    if reps % 2 !=  0:
+        count_down(work_sec)
+    # #If its the 8th rep:
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    # #if its the 2nd/4th/6th rep:
+    if reps % 2 == 0:
+        count_down(short_break_sec)
+    
+
+    print(reps)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -29,6 +47,8 @@ def count_down(count):
     if count > 0:
         canvas.itemconfig(timer_text, text=f"{count_minute}:{count_sec}")
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
